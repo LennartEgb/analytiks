8418d60695c24e937e9cdb582ec09a7288ce376b
Lennart Egbers <lennartegbers@hotmail.com>
2021-01-11 13:50:15 +0100

    Tests adjusted and are more readable now

--- analytiks/src/test/java/de/lennartegb/analytiks/AnalytiksTest.kt
+++ analytiks/src/test/java/de/lennartegb/analytiks/AnalytiksTest.kt
@@ -30,9 +30,7 @@ internal class AnalytiksTest {
     @Nested
     inner class RegisterService {
 
-        private fun getTestService(name: String) = object : Analytiks.Service {
-            override val name: String
-                get() = name
+        private inner class TestService(override val name: String) : Analytiks.Service {
             override val isEnabled: Boolean
                 get() = fail("Should never be called")
 
@@ -43,23 +41,23 @@ internal class AnalytiksTest {
 
         @Test
         fun `with one service and serviceCount returns 1`() {
-            Analytiks.registerService(getTestService(name = "TEST_1"))
+            Analytiks.registerService(TestService(name = "TEST_1"))
             assertEquals(expected = 1, actual = Analytiks.serviceCount)
         }
 
         @Test
         fun `with two different services and serviceCount returns 2`() {
-            Analytiks.registerService(getTestService(name = "TEST_1"))
-            Analytiks.registerService(getTestService(name = "TEST_2"))
+            Analytiks.registerService(TestService(name = "TEST_1"))
+            Analytiks.registerService(TestService(name = "TEST_2"))
             assertEquals(expected = 2, actual = Analytiks.serviceCount)
         }
 
         @Test
         fun `with two equal services throws IllegalArgumentException`() {
-            Analytiks.registerService(getTestService(name = "TEST_1"))
+            Analytiks.registerService(TestService(name = "TEST_1"))
 
             assertThrows<AlreadyRegisteredService> {
-                Analytiks.registerService(getTestService(name = "TEST_1"))
+                Analytiks.registerService(TestService(name = "TEST_1"))
             }
         }
 
@@ -70,29 +68,28 @@ internal class AnalytiksTest {
 
         private val testEvent = Analytiks.Action.Event("TEST_EVENT")
 
-        private fun getTestService(isEnabled: Boolean, onTrack: () -> Unit) =
-            object : Analytiks.Service {
-                override val name: String
-                    get() = "TEST_SERVICE"
-                override val isEnabled: Boolean = isEnabled
-
-                override fun track(action: Analytiks.Action) {
-                    when (action) {
-                        is Analytiks.Action.Event -> onTrack()
-                        is Analytiks.Action.View -> fail("View tracking should never be called when tracking event!")
-                    }
+        private inner class TestService(
+            override val name: String = "TEST_SERVICE",
+            override val isEnabled: Boolean,
+            private val onTrack: () -> Unit,
+        ) : Analytiks.Service {
+            override fun track(action: Analytiks.Action) {
+                when (action) {
+                    is Analytiks.Action.Event -> onTrack()
+                    is Analytiks.Action.View -> fail("View tracking should never be called when tracking event!")
                 }
             }
+        }
 
         @Test
         fun `if service is enabled calls event tracking of child services`() {
             var trackingIsCalled = false
-            Analytiks.registerService(
-                getTestService(
-                    isEnabled = true,
-                    onTrack = { trackingIsCalled = true })
-            )
+            val service = TestService(isEnabled = true, onTrack = { trackingIsCalled = true })
+
+            Analytiks.registerService(service)
+
             Analytiks.track(testEvent)
+
             assertTrue(
                 actual = trackingIsCalled,
                 message = "Tracking should have been called and changed the test boolean"
@@ -102,12 +99,12 @@ internal class AnalytiksTest {
         @Test
         fun `if service is not enabled does not call event tracking of child services`() {
             var trackingIsCalled = false
-            Analytiks.registerService(
-                getTestService(
-                    isEnabled = false,
-                    onTrack = { trackingIsCalled = true })
-            )
+            val service = TestService(isEnabled = false, onTrack = { trackingIsCalled = true })
+
+            Analytiks.registerService(service)
+
             Analytiks.track(testEvent)
+
             assertFalse(
                 actual = trackingIsCalled,
                 message = "Tracking should have not been called and not changed the test boolean"
@@ -121,29 +118,28 @@ internal class AnalytiksTest {
 
         private val testView = Analytiks.Action.View(name = "TEST_VIEW")
 
-        private fun getTestService(isEnabled: Boolean, onTrack: () -> Unit) =
-            object : Analytiks.Service {
-                override val name: String
-                    get() = "TEST_SERVICE"
-                override val isEnabled: Boolean = isEnabled
-
-                override fun track(action: Analytiks.Action) {
-                    when (action) {
-                        is Analytiks.Action.Event -> fail("Event tracking should never be called when tracking view!")
-                        is Analytiks.Action.View -> onTrack()
-                    }
+        private inner class TestService(
+            override val name: String = "TEST_SERVICE",
+            override val isEnabled: Boolean,
+            private val onTrack: () -> Unit
+        ) : Analytiks.Service {
+            override fun track(action: Analytiks.Action) {
+                when (action) {
+                    is Analytiks.Action.Event -> fail("Event tracking should never be called when tracking view!")
+                    is Analytiks.Action.View -> onTrack()
                 }
             }
+        }
 
         @Test
         fun `if service is enabled calls view tracking of child services`() {
             var trackingIsCalled = false
-            Analytiks.registerService(
-                getTestService(
-                    isEnabled = true,
-                    onTrack = { trackingIsCalled = true })
-            )
+            val service = TestService(isEnabled = true, onTrack = { trackingIsCalled = true })
+
+            Analytiks.registerService(service)
+
             Analytiks.track(testView)
+
             assertTrue(
                 actual = trackingIsCalled,
                 message = "Tracking should have been called and changed the test boolean"
@@ -153,12 +149,12 @@ internal class AnalytiksTest {
         @Test
         fun `if service is not enabled does not call view tracking of child services`() {
             var trackingIsCalled = false
-            Analytiks.registerService(
-                getTestService(
-                    isEnabled = false,
-                    onTrack = { trackingIsCalled = true })
-            )
+            val service = TestService(isEnabled = false, onTrack = { trackingIsCalled = true })
+
+            Analytiks.registerService(service)
+
             Analytiks.track(testView)
+
             assertFalse(
                 actual = trackingIsCalled,
                 message = "Tracking should have not been called and not changed the test boolean"
